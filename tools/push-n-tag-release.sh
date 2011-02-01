79c7c535ad1a49d31928c9e3f9eda5f2949937ad
Robert Nelson <robertcnelson@gmail.com>
2011-01-31 22:51:09 -0600

    tags should be m too
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/push-n-tag-release.sh
+++ tools/push-n-tag-release.sh
@@ -10,15 +10,15 @@ if [ -e ${DIR}/version.sh ]; then
 
         if [ "${RC_PATCH}" ]; then
 		git commit -a -m "${RC_KERNEL}${RC_PATCH}-${BUILD} release" -s
-		git tag -a "${RC_KERNEL}${RC_PATCH}-${BUILD}"
+		git tag -a -m "${RC_KERNEL}${RC_PATCH}-${BUILD}"
 		git push origin --tags
 	else if [ "${STABLE_PATCH}" ] ; then
 		git commit -a -m "${KERNEL_REL}.${STABLE_PATCH}-${BUILD} release" -s
-		git tag -a "${KERNEL_REL}.${STABLE_PATCH}-${BUILD}"
+		git tag -a -m "${KERNEL_REL}.${STABLE_PATCH}-${BUILD}"
 		git push origin --tags
 	else
 		git commit -a -m "${KERNEL_REL}-${BUILD} release" -s
-		git tag -a "${KERNEL_REL}.${STABLE_PATCH}"
+		git tag -a -m "${KERNEL_REL}.${STABLE_PATCH}"
 		git push origin --tags
 	fi
 	fi
