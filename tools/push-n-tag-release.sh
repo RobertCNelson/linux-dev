4d40398b5ba0657b490fdd2bda8860aa351232b6
Robert Nelson <robertcnelson@gmail.com>
2011-02-07 18:59:05 -0600

    should make tag pushes eaiser
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/push-n-tag-release.sh
+++ tools/push-n-tag-release.sh
@@ -12,14 +12,17 @@ if [ -e ${DIR}/version.sh ]; then
 		git commit -a -m "${RC_KERNEL}${RC_PATCH}-${BUILD} release" -s
 		git tag -a "${RC_KERNEL}${RC_PATCH}-${BUILD}" -m "${RC_KERNEL}${RC_PATCH}-${BUILD}"
 		git push origin --tags
+		git push origin
 	else if [ "${STABLE_PATCH}" ] ; then
 		git commit -a -m "${KERNEL_REL}.${STABLE_PATCH}-${BUILD} release" -s
 		git tag -a "${KERNEL_REL}.${STABLE_PATCH}-${BUILD}" -m "${KERNEL_REL}.${STABLE_PATCH}-${BUILD}"
 		git push origin --tags
+		git push origin
 	else
 		git commit -a -m "${KERNEL_REL}-${BUILD} release" -s
 		git tag -a "${KERNEL_REL}.${STABLE_PATCH}" -m "${KERNEL_REL}.${STABLE_PATCH}"
 		git push origin --tags
+		git push origin
 	fi
 	fi
 fi
