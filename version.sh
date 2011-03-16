074c088185e66ff8bd919cc8401441f370ed99ed
Robert Nelson <robertcnelson@gmail.com>
2011-03-16 11:10:28 -0500

    merge to 2.6.38-git1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,10 +4,10 @@ unset BUILD
 
 KERNEL_REL=2.6.38
 #STABLE_PATCH=.1
-#PRE_RC=2.6.37-git18
+PRE_RC=2.6.38-git1
 #RC_KERNEL=2.6.38
 #RC_PATCH=-rc8
-ABI=5
+ABI=1
 
 if [ "${NO_DEVTMPS}" ] ; then
 BUILD=dold${ABI}
