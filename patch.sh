94dd2fbc2751691f9de4f9a45a4ed0dd1bd54105
Robert Nelson <robertcnelson@gmail.com>
2011-03-29 20:53:00 -0500

    beagle, refix my hp monitor with 2.6.38
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -16,9 +16,7 @@ patch -s -p1 < "${DIR}/patches/trivial/0001-staging-add-airlink-awll7025-id-for-
 #Bisected from 2.6.35 -> 2.6.36 to find this..
 #This commit breaks some lcd monitors..
 #rcn-ee Feb 26, 2011...
-#Still needs more work for 2.6.38, causes:
-#[   14.962829] omapdss DISPC error: GFX_FIFO_UNDERFLOW, disabling GFX
-#patch -s -p1 < "${DIR}/patches/trivial/0001-Revert-OMAP-DSS2-OMAPFB-swap-front-and-back-porches-.patch"
+patch -s -p1 < "${DIR}/patches/trivial/0001-Revert-OMAP-DSS2-OMAPFB-swap-front-and-back-porches-.patch"
 
 }
 
--- version.sh
+++ version.sh
@@ -7,7 +7,7 @@ STABLE_PATCH=2
 #PRE_RC=2.6.37-git18
 #RC_KERNEL=2.6.38
 #RC_PATCH=-rc8
-ABI=8.1
+ABI=8.2
 
 if [ "${NO_DEVTMPS}" ] ; then
 BUILD=dold${ABI}
