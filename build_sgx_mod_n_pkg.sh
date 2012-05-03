da3795e68e7f578204d86e3795f1efa60be04cc5
Robert Nelson <robertcnelson@gmail.com>
2012-05-03 18:57:13 -0500

    sgx: less hardcoding
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_sgx_mod_n_pkg.sh
+++ build_sgx_mod_n_pkg.sh
@@ -28,6 +28,18 @@ DIR=$PWD
 
 SGX_SHA="origin/master"
 
+set_sgx_make_vars () {
+	GRAPHICS_PATH="GRAPHICS_INSTALL_DIR="${DIR}/ti-sdk-pvr/Graphics_SDK/""
+	KERNEL_PATH="KERNEL_INSTALL_DIR="${DIR}/KERNEL""
+	USER_VAR="HOME=/home/${USER}"
+	CSTOOL_PREFIX=${CC##*/}
+
+	#Will probally have to revist this one later...
+	CSTOOL_DIR=$(echo ${CC} | awk -F "/bin/${CSTOOL_PREFIX}" '{print $1}')
+
+	CROSS="CSTOOL_PREFIX=${CSTOOL_PREFIX} CSTOOL_DIR=${CSTOOL_DIR}"
+}
+
 git_sgx_modules () {
 	if [ ! -f "${DIR}/ti-sdk-pvr/.git/config" ] ; then
 		git clone git://github.com/RobertCNelson/ti-sdk-pvr.git
@@ -49,10 +61,27 @@ git_sgx_modules () {
 
 build_sgx_modules () {
 	cd "${DIR}/ti-sdk-pvr/Graphics_SDK/"
-	make HOME=/home/${USER} CSTOOL_DIR=/opt/github/linaro-tools/cross-gcc/build/sysroot/home/voodoo/opt/gcc-linaro-cross CSTOOL_PREFIX=arm-linux-gnueabi- KERNEL_INSTALL_DIR="${DIR}/KERNEL" BUILD=release OMAPES=3.x FBDEV=yes SUPPORT_XORG=1 all
+	make ${GRAPHICS_PATH} ${KERNEL_PATH} ${USER_VAR} ${CROSS} BUILD="$1" OMAPES="$2" FBDEV="$3" SUPPORT_XORG="$4" "$5"
 	cd ${DIR}/
 }
 
-git_sgx_modules
-build_sgx_modules
+if [ -e ${DIR}/system.sh ] ; then
+	source system.sh
+	source version.sh
+
+	set_sgx_make_vars
+
+	git_sgx_modules
+	build_sgx_modules release 3.x yes 1 all
+#	build_sgx_modules release 5.x yes 1 all
+#	build_sgx_modules release 6.x yes 1 all
+#	build_sgx_modules release 8.x yes 1 all
+else
+	echo ""
+	echo "ERROR: Missing (your system) specific system.sh, please copy system.sh.sample to system.sh and edit as needed."
+	echo ""
+	echo "example: cp system.sh.sample system.sh"
+	echo "example: gedit system.sh"
+	echo ""
+fi
 
