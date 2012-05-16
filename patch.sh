b575195aa8e54d385f516b7ab7c13f8943355a20
Robert Nelson <robertcnelson@gmail.com>
2012-05-16 18:36:02 -0500

    testing: bone disable OPP120
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -1167,7 +1167,7 @@ function am33x-cleanup {
 	git am "${DIR}/patches/beaglebone/0040-beaglebone-export-SPI2-as-spidev-when-no-capes-are-u.patch"
 
 	git am "${DIR}/patches/beaglebone/0001-bone-disable-OPPTURBO.patch"
-
+	git am "${DIR}/patches/beaglebone/0002-bone-disable-OPP120.patch"
 
 #git am "${DIR}/patches/beaglebone/0001-ARM-omap-am335x-BeagleBone-userspace-SPI-support.patch"
 #	git am "${DIR}/patches/fixes/0001-rt2x00-Add-support-for-D-Link-DWA-127-to-rt2800usb.patch"
