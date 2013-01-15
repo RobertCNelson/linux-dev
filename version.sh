9a21f497ae9f5d4179348a2409ed0ad0abcd64cb
Robert Nelson <robertcnelson@gmail.com>
2013-01-15 15:44:22 -0600

    bump to v3.7.2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ config="omap2plus_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.7
-KERNEL_TAG=${KERNEL_REL}.1
-BUILD=bone4
+KERNEL_TAG=${KERNEL_REL}.2
+BUILD=bone4.1
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
