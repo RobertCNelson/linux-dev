f13aa9f7a13286644bb52945b27ab2aef74f789e
Robert Nelson <robertcnelson@gmail.com>
2013-01-25 10:11:19 -0600

    scripts: merge with master
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/git.sh
+++ scripts/git.sh
@@ -140,29 +140,6 @@ git_kernel () {
 	git describe
 
 	cd ${DIR}/
-
-	if [ "${IMX_BOOTLETS}" ] ; then
-		if [ ! -f "${DIR}"/ignore/imx-bootlets/.git/config ] ; then
-			rm -rf "${DIR}"/ignore/imx-bootlets || true
-			mkdir -p "${DIR}"/ignore/imx-bootlets
-			git clone ${imx_bootlets_repo} "${DIR}"/ignore/imx-bootlets
-		fi
-
-		if [ "${imx_bootlets_tag}" ] ; then
-			cd "${DIR}"/ignore/imx-bootlets/
-			git checkout origin/master -b tmp-master
-			git branch -D master &>/dev/null || true
-			git branch -D tmp &>/dev/null || true
-
-			git checkout origin/master -b master
-			git branch -D tmp-master &>/dev/null || true
-
-			git pull ${GIT_OPTS} || true
-			git checkout origin/${imx_bootlets_tag} -b tmp
-			cd ${DIR}/
-		fi
-	fi
-
 }
 
 source ${DIR}/version.sh
@@ -171,11 +148,9 @@ source ${DIR}/system.sh
 if [ "${GIT_OVER_HTTP}" ] ; then
 	torvalds_linux="http://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 	linux_stable="http://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git"
-	imx_bootlets_repo="https://github.com/RobertCNelson/imx-bootlets.git"
 else
 	torvalds_linux="git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 	linux_stable="git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git"
-	imx_bootlets_repo="git://github.com/RobertCNelson/imx-bootlets.git"
 fi
 
 unset ON_MASTER
--- tools/install_image.sh
+++ tools/install_image.sh
@@ -182,18 +182,6 @@ mmc_write_boot () {
 	mmc_find_rootfs
 }
 
-mmc_write_imx_bootlets () {
-	echo "Installing ${KERNEL_UTS}.sd_mmc_bootstream.raw to boot partition"
-	echo "-----------------------------"
-
-	if [ -f "${DIR}/deploy/${KERNEL_UTS}.sd_mmc_bootstream.raw" ] ; then
-		sudo dd if="${DIR}/deploy/${KERNEL_UTS}.sd_mmc_bootstream.raw" of=${MMC}${PARTITION_PREFIX}${BOOT_PARITION}
-	fi
-	sync
-	sync
-	mmc_find_rootfs
-}
-
 mmc_mount_boot () {
 	if [ ! -d "${DIR}/deploy/disk/" ] ; then
 		mkdir -p "${DIR}/deploy/disk/"
@@ -230,11 +218,7 @@ unmount_partitions () {
 	done
 
 	mkdir -p "${DIR}/deploy/disk/"
-	if [ "${imx_bootlets_target}" ] ; then
-		mmc_write_imx_bootlets
-	else
-		mmc_mount_boot
-	fi
+	mmc_mount_boot
 }
 
 debug_display_partitions () {
