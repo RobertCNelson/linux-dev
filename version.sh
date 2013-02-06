747e730367466c252acaff319017896aafe593fe
Robert Nelson <robertcnelson@gmail.com>
2013-02-05 19:06:17 -0600

    Revert "sync with angstrom"
    This reverts commit a70c4f0a7229a854f7a6abe3e32404baa291248a.

--- version.sh
+++ version.sh
@@ -24,10 +24,10 @@ config="omap2plus_defconfig"
 #Kernel/Build
 KERNEL_REL=3.8
 KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=bone1.1
+BUILD=bone1
 
 #v3.X-rcX + upto SHA
-KERNEL_SHA="2e51b231a8d716ea5aacde0bd95ac789cea195b0"
+#KERNEL_SHA="66e2d3e8c2294543a6f0453d974940171829e7dd"
 
 #git branch
 BRANCH="am33x-v3.8"
