c85d276c5c4ac175be30650d214788aeae4a99ca
Robert Nelson <robertcnelson@gmail.com>
2013-02-10 19:58:23 -0600

    3.8-rc7-bone4 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -23,11 +23,11 @@ config="omap2plus_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.8
-KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=bone3.1
+KERNEL_TAG=${KERNEL_REL}-rc7
+BUILD=bone4
 
 #v3.X-rcX + upto SHA
-KERNEL_SHA="1589a3e7777631ff56dd58cd7dcdf275185e62b5"
+#KERNEL_SHA="1589a3e7777631ff56dd58cd7dcdf275185e62b5"
 
 #git branch
 BRANCH="am33x-v3.8"
