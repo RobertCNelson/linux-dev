2cddaf08da7c4453e41375c70ba4582bc9db7693
Robert Nelson <robertcnelson@gmail.com>
2013-02-20 18:14:11 -0600

    merge to: https://github.com/RobertCNelson/linux/commit/8ec4942212a6d337982967778a3dc3b60aea782e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,10 +24,10 @@ config="omap2plus_defconfig"
 #Kernel/Build
 KERNEL_REL=3.8
 KERNEL_TAG=${KERNEL_REL}
-BUILD=d0.3
+BUILD=d0.4
 
 #v3.X-rcX + upto SHA
-KERNEL_SHA="55529fa5762462beb39f913c5277cb96c7be0858"
+KERNEL_SHA="8ec4942212a6d337982967778a3dc3b60aea782e"
 
 #git branch
 #BRANCH="v3.9.x"
