71476084f17f9c823a36df2eb7df93993270a033
Robert Nelson <robertcnelson@gmail.com>
2013-09-18 09:15:46 -0500

    sync with v3.12.x branch
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ linaro_toolchain="cortex_gcc_4_8"
 #Kernel/Build
 KERNEL_REL=3.12
 KERNEL_TAG=${KERNEL_REL}-rc1
-BUILD=armv7-d0.29
+BUILD=armv7-d0.30
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="bdbdfdef5766c2a60185e946df242f1bc0d37c09"
