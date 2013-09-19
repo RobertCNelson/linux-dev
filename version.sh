cb690e108c6877958760e79361696615903d20e9
Robert Nelson <robertcnelson@gmail.com>
2013-09-19 10:56:19 -0500

    beagle xm: update the twl bit2 settings dvi-enable
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ linaro_toolchain="cortex_gcc_4_8"
 #Kernel/Build
 KERNEL_REL=3.12
 KERNEL_TAG=${KERNEL_REL}-rc1
-BUILD=armv7-d0.37
+BUILD=armv7-d0.38
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA="ed24fee24a6be9568b1ee30209bafe4dad66be0e"
