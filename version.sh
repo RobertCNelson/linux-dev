4ad183aa783940e98d81d567c1fbf238b0670212
Robert Nelson <robertcnelson@gmail.com>
2013-10-20 10:49:20 -0500

    3.12-rc6-bone7 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,11 +26,11 @@ linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.12
-KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=bone6.2
+KERNEL_TAG=${KERNEL_REL}-rc6
+BUILD=bone7
 
 #v3.X-rcX + upto SHA
-KERNEL_SHA="04919afb85c8f007b7326c4da5eb61c52e91b9c7"
+#KERNEL_SHA="04919afb85c8f007b7326c4da5eb61c52e91b9c7"
 
 #git branch
 BRANCH="am33x-v3.12"
