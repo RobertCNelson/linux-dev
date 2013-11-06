54b2d14a7f8a1c4615651e623ed62e23d4102879
Robert Nelson <robertcnelson@gmail.com>
2013-11-06 16:26:22 -0600

    config: sync with debians v3.12-rc7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ linaro_toolchain="cortex_gcc_4_8"
 #Kernel/Build
 KERNEL_REL=3.12
 KERNEL_TAG=${KERNEL_REL}
-BUILD=armv7-d0.1
+BUILD=armv7-d0.2
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA="be408cd3e1fef73e9408b196a79b9934697fe3b1"
