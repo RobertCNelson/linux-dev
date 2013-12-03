3102380132d431296766e8d879e74db3d91dc634
Robert Nelson <robertcnelson@gmail.com>
2013-12-03 15:44:10 -0600

    imx: merge next chanages
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ linaro_toolchain="cortex_gcc_4_8"
 #Kernel/Build
 KERNEL_REL=3.13
 KERNEL_TAG=${KERNEL_REL}-rc2
-BUILD=armv7-devel-r26
+BUILD=armv7-devel-r27
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA="dea4f48a0a301b23c65af8e4fe8ccf360c272fbf"
