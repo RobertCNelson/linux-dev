c274a0bc3fd079dabc20ce6acfa5b2396abca462
Robert Nelson <robertcnelson@gmail.com>
2013-12-12 11:45:24 -0600

    kernel bump: v3.12.5
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.12
-KERNEL_TAG=${KERNEL_REL}.4
-BUILD=bone9.1
+KERNEL_TAG=${KERNEL_REL}.5
+BUILD=bone9.2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="e6036c0b88962df82a8853971b86a55f09faef40"
