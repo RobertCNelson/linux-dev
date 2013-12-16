c5b6de805ab5ac3bb70801b6772f5d806b437299
Robert Nelson <robertcnelson@gmail.com>
2013-12-15 18:50:02 -0600

    kernel bump: v3.13-rc4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,11 +26,11 @@ linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.13
-KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=armv7-devel-r38
+KERNEL_TAG=${KERNEL_REL}-rc4
+BUILD=armv7-devel-r39
 
 #v3.X-rcX + upto SHA
-KERNEL_SHA="ca3367518b89b7c1e593dee82a4a4c9bbdfa8695"
+#KERNEL_SHA="ca3367518b89b7c1e593dee82a4a4c9bbdfa8695"
 
 #git branch
 #BRANCH="v3.13.x"
