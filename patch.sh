feb6e792f5b6d5944d99af5d23ea7075e102a7c2
Robert Nelson <robertcnelson@gmail.com>
2013-12-18 14:48:12 -0600

    and this patch
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -234,6 +234,7 @@ sgx () {
 	${git} "${DIR}/patches/sgx/0003-arm-Export-cache-flush-management-symbols-when-MULTI.patch"
 	${git} "${DIR}/patches/sgx/0004-hack-port-da8xx-changes-from-ti-3.12-repo.patch"
 	${git} "${DIR}/patches/sgx/0005-Revert-drm-remove-procfs-code-take-2.patch"
+	${git} "${DIR}/patches/sgx/0006-Changes-according-to-TI-for-SGX-support.patch"
 }
 
 sgx_blob () {
