de1d9d71be3f769f2798b33889f50ba4295ec653
Robert Nelson <robertcnelson@gmail.com>
2013-12-20 09:28:27 -0600

    scripts: sync with master of: git://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-checker.sh
+++ tools/config-checker.sh
@@ -2,6 +2,24 @@
 
 DIR=$PWD
 
+check_config_builtin () {
+	unset test_config
+	test_config=$(grep "${config}=y" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x" ] ; then
+		echo "Config: [${config}=y] not enabled"
+		exit
+	fi
+}
+
+check_config_disabled () {
+	unset test_config
+	test_config=$(grep "${config} is not set" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x" ] ; then
+		echo "Disable config: [${config}]"
+		exit
+	fi
+}
+
 check_config () {
 	unset test_config
 	test_config=$(grep "${config}=" ${DIR}/patches/defconfig || true)
@@ -11,7 +29,55 @@ check_config () {
 	fi
 }
 
-#2013-12-19: make sure zram is enabled...
+#systemd : http://cgit.freedesktop.org/systemd/systemd/tree/README#n36
+config="CONFIG_DEVTMPFS"
+check_config_builtin
+config="CONFIG_CGROUPS"
+check_config_builtin
+config="CONFIG_INOTIFY_USER"
+check_config_builtin
+config="CONFIG_SIGNALFD"
+check_config_builtin
+config="CONFIG_TIMERFD"
+check_config_builtin
+config="CONFIG_EPOLL"
+check_config_builtin
+config="CONFIG_NET"
+check_config_builtin
+config="CONFIG_SYSFS"
+check_config_builtin
+config="CONFIG_PROC_FS"
+check_config_builtin
+config="CONFIG_SYSFS_DEPRECATED"
+check_config_disabled
+#CONFIG_UEVENT_HELPER_PATH=""
+config="CONFIG_FW_LOADER_USER_HELPER"
+check_config_disabled
+#CONFIG_DMIID
+config="CONFIG_FHANDLE"
+check_config_builtin
+config="CONFIG_BLK_DEV_BSG"
+check_config_builtin
+config="CONFIG_IPV6"
+check_config_builtin
+config="CONFIG_AUTOFS4_FS"
+check_config_builtin
+config="CONFIG_TMPFS_POSIX_ACL"
+check_config_builtin
+config="CONFIG_TMPFS_XATTR"
+check_config_builtin
+config="CONFIG_SECCOMP"
+check_config_builtin
+config="CONFIG_SCHEDSTATS"
+check_config_builtin
+config="CONFIG_SCHED_DEBUG"
+check_config_builtin
+config="CONFIG_AUDIT"
+check_config_disabled
+
+#zram
+config="CONFIG_ZSMALLOC"
+check_config
 config="CONFIG_ZRAM"
 check_config
 
