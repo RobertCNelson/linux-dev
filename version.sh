5f199fca1cac4565d4a63d27956c326b866000fe
Cody Lacey <clacey@ti.com>
2014-01-16 09:12:03 -0600

    3.12: Fix Writing to Read-Only Bit
    Fix writing to read-only bit in davinci-mcasp.c
    
    import of: https://github.com/beagleboard/kernel/commit/0269a35a2de49bbbb772ec093dd35a3541f5ab22
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -29,7 +29,7 @@ toolchain="cortex_gcc_4_8"
 #Kernel/Build
 KERNEL_REL=3.12
 KERNEL_TAG=${KERNEL_REL}.7
-BUILD=bone11.3
+BUILD=bone11.4
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="e6036c0b88962df82a8853971b86a55f09faef40"
