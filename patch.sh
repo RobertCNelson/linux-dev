69be0c6bd3cef30346e4f00614bb65eafb115998
Robert Nelson <robertcnelson@gmail.com>
2014-04-07 10:35:07 -0500

    cleanup: saucy
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -164,15 +164,6 @@ next () {
 	${git} "${DIR}/patches/next/0009-arm-dts-sun4i-a10-olinuxino-lime-enable-mmc0.patch"
 }
 
-saucy () {
-	echo "dir: saucy"
-	#need to be re-tested with v3.14-rcX
-	#Ubuntu Saucy: so Ubuntu decided to enable almost every Warning -> Error option...
-	${git} "${DIR}/patches/saucy/0001-saucy-error-variable-ilace-set-but-not-used-Werror-u.patch"
-	${git} "${DIR}/patches/saucy/0002-saucy-disable-Werror-pointer-sign.patch"
-	${git} "${DIR}/patches/saucy/0003-saucy-disable-stack-protector.patch"
-}
-
 #revert
 drivers
 #imx_next
@@ -188,6 +179,5 @@ omap3_beagle_xm_rework
 fixes
 vivante
 next
-#saucy
 
 echo "patch.sh ran successful"
