d103621c25fbebd9e83cf1b68e3b1f4767b45afd
Robert Nelson <robertcnelson@gmail.com>
2014-07-20 16:37:01 -0500

    import: imx_next
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=3.16
 KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=armv7-devel-r45
+BUILD=armv7-devel-r46
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA="d057190925d994b808e1d07e6c76b90a32caac77"
