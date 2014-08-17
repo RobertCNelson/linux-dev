b1b13c60e0afbdfbe1e8f296e1b718cc5345fef5
Robert Nelson <robertcnelson@gmail.com>
2014-08-17 17:15:42 -0500

    kernel bump: v3.17-rc1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,12 +25,12 @@ config="multi_v7_defconfig"
 toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
-KERNEL_REL=3.16
-KERNEL_TAG=${KERNEL_REL}
-BUILD=armv7-devel-r16
+KERNEL_REL=3.17
+KERNEL_TAG=${KERNEL_REL}-rc1
+BUILD=armv7-devel-r17
 
 #v3.X-rcX + upto SHA
-KERNEL_SHA="c9d26423e56ce1ab4d786f92aebecf859d419293"
+#KERNEL_SHA="c9d26423e56ce1ab4d786f92aebecf859d419293"
 
 #git branch
 #BRANCH="v3.17.x"
