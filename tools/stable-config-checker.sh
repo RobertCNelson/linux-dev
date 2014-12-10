420e38c49c96f3cdd095209fad60d7b4159cb344
Robert Nelson <robertcnelson@gmail.com>
2014-12-10 12:50:32 -0600

    add stable config checker
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ tools/stable-config-checker.sh
@@ -0,0 +1,95 @@
+#!/bin/sh -e
+
+DIR=$PWD
+
+check_config_value () {
+	unset test_config
+	test_config=$(grep "${config}=" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x" ] ; then
+		echo "echo ${config}=${value} >> ./KERNEL/.config"
+	else
+		if [ ! "x${test_config}" = "x${config}=${value}" ] ; then
+			echo "sed -i -e 's:${test_config}:${config}=${value}:g' ./KERNEL/.config"
+		fi
+	fi
+}
+
+check_config_builtin () {
+	unset test_config
+	test_config=$(grep "${config}=y" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x" ] ; then
+		echo "echo ${config}=y >> ./KERNEL/.config"
+	fi
+}
+
+check_config_module () {
+	unset test_config
+	test_config=$(grep "${config}=y" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x${config}=y" ] ; then
+		echo "sed -i -e 's:${config}=y:${config}=m:g' ./KERNEL/.config"
+	else
+		unset test_config
+		test_config=$(grep "${config}=" ${DIR}/patches/defconfig || true)
+		if [ "x${test_config}" = "x" ] ; then
+			echo "echo ${config}=m >> ./KERNEL/.config"
+		fi
+	fi
+}
+
+check_config () {
+	unset test_config
+	test_config=$(grep "${config}=" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x" ] ; then
+		echo "echo ${config}=y >> ./KERNEL/.config"
+		echo "echo ${config}=m >> ./KERNEL/.config"
+	fi
+}
+
+check_config_disable () {
+	unset test_config
+	test_config=$(grep "${config} is not set" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x" ] ; then
+		unset test_config
+		test_config=$(grep "${config}=y" ${DIR}/patches/defconfig || true)
+		if [ "x${test_config}" = "x${config}=y" ] ; then
+			echo "sed -i -e 's:${config}=y:# ${config} is not set:g' ./KERNEL/.config"
+		else
+			echo "sed -i -e 's:${config}=m:# ${config} is not set:g' ./KERNEL/.config"
+		fi
+	fi
+}
+
+check_if_set_then_set_module () {
+	unset test_config
+	test_config=$(grep "${if_config}=y" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x${if_config}=y" ] ; then
+		check_config_module
+	fi
+}
+
+check_if_set_then_set () {
+	unset test_config
+	test_config=$(grep "${if_config}=y" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x${if_config}=y" ] ; then
+		check_config_builtin
+	fi
+}
+
+check_if_set_then_disable () {
+	unset test_config
+	test_config=$(grep "${if_config}=y" ${DIR}/patches/defconfig || true)
+	if [ "x${test_config}" = "x${if_config}=y" ] ; then
+		check_config_disable
+	fi
+}
+
+#v3.18:
+#Beaglebone: allow PTP timestamping in hardware
+config="CONFIG_TI_CPTS"
+check_config_builtin
+
+#office: use these for testing..
+config="CONFIG_HID_LOGITECH_DJ"
+check_config_builtin
+
+#
