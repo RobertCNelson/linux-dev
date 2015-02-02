8d2b778c43b6b89b46530ff44c58fa5858375fe3
Robert Nelson <robertcnelson@gmail.com>
2015-02-02 09:11:57 -0600

    kernel bump: v3.19-rc7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -21,12 +21,12 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=3.19
-KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=armv7-devel-r57
+KERNEL_TAG=${KERNEL_REL}-rc7
+BUILD=armv7-devel-r58
 
 #v3.X-rcX + upto SHA
-prev_KERNEL_SHA="00845eb968ead28007338b2bb852b8beef816583"
-KERNEL_SHA="fba7e99458b606dc9a84332d38cd40e9d367633f"
+#prev_KERNEL_SHA="00845eb968ead28007338b2bb852b8beef816583"
+#KERNEL_SHA="fba7e99458b606dc9a84332d38cd40e9d367633f"
 
 #git branch
 #BRANCH="v3.19.x"
