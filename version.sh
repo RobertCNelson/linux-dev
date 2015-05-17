4ecb4ec6edfe4bad14b94a6a304c20229fc6f4a6
Robert Nelson <robertcnelson@gmail.com>
2015-05-17 18:26:23 -0500

    armv7-devel-r38: merge to: https://github.com/torvalds/linux/commit/c0655fe9b0901a968800f56687be3c62b4cce5d2
    Compare: https://github.com/torvalds/linux/compare/110bc76729d448fdbcb5cdb63b83d9fd65ce5e26...c0655fe9b0901a968800f56687be3c62b4cce5d2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -14,11 +14,11 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=armv7-devel-r37
+BUILD=armv7-devel-r38
 
 #v3.X-rcX + upto SHA
-prev_KERNEL_SHA="6c9d370c16aad97cfc6de68666634eaabac2b048"
-KERNEL_SHA="110bc76729d448fdbcb5cdb63b83d9fd65ce5e26"
+prev_KERNEL_SHA="110bc76729d448fdbcb5cdb63b83d9fd65ce5e26"
+KERNEL_SHA="c0655fe9b0901a968800f56687be3c62b4cce5d2"
 
 #git branch
 #BRANCH="v4.1.x"
