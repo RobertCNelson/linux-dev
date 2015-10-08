3853d2078970253b167e6601a94ee1d704e68516
Robert Nelson <robertcnelson@gmail.com>
2015-10-08 14:56:04 -0500

    resync reverts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -114,15 +114,14 @@ reverts () {
 		start_cleanup
 	fi
 
-	${git} "${DIR}/patches/reverts/0001-Revert-spi-spidev-add-compatible-value-for-LTC2488.patch"
-	${git} "${DIR}/patches/reverts/0002-Revert-spi-spidev-Warn-loudly-if-instantiated-from-D.patch"
+	${git} "${DIR}/patches/reverts/0001-Revert-spi-spidev-Warn-loudly-if-instantiated-from-D.patch"
 	#udoo:
-	${git} "${DIR}/patches/reverts/0003-Revert-usb-chipidea-usbmisc_imx-delete-clock-informa.patch"
+	${git} "${DIR}/patches/reverts/0002-Revert-usb-chipidea-usbmisc_imx-delete-clock-informa.patch"
 	#am335x causing random reboots...
 #	${git} "${DIR}/patches/reverts/0004-Revert-usb-musb-dsps-just-start-polling-already.patch"
 
 	if [ "x${regenerate}" = "xenable" ] ; then
-		number=4
+		number=2
 		cleanup
 	fi
 }
