4c74733431ff1c3d6e6fc8b9bcd2ab8bbaeaa21a
Robert Nelson <robertcnelson@gmail.com>
2015-10-21 18:59:36 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/host_det.sh
+++ tools/host_det.sh
@@ -248,19 +248,19 @@ debian_regs () {
 			deb_distro="jessie"
 			;;
 		isadora)
-			#eol
+			#9
 			deb_distro="lucid"
 			;;
 		julia)
-			#eol
+			#10
 			deb_distro="maverick"
 			;;
 		katya)
-			#eol
+			#11
 			deb_distro="natty"
 			;;
 		lisa)
-			#eol
+			#12
 			deb_distro="oneiric"
 			;;
 		maya)
@@ -268,15 +268,15 @@ debian_regs () {
 			deb_distro="precise"
 			;;
 		nadia)
-			#eol
+			#14
 			deb_distro="quantal"
 			;;
 		olivia)
-			#eol
+			#15
 			deb_distro="raring"
 			;;
 		petra)
-			#eol
+			#16
 			deb_distro="saucy"
 			;;
 		qiana)
@@ -311,6 +311,10 @@ debian_regs () {
 			#9 stretch: https://wiki.debian.org/DebianStretch
 			unset warn_eol_distro
 			;;
+		xenial)
+			#16.04 trusty: (EOL: April 20xx) lts: xenial -> xyz
+			unset warn_eol_distro
+			;;
 		vivid|wily)
 			#15.04 vivid: (EOL: January 2016)
 			#15.10 wily: (EOL: July 2016)
@@ -322,7 +326,7 @@ debian_regs () {
 			stop_pkg_search=1
 			;;
 		trusty)
-			#14.04 trusty: (EOL: April 2019) lts: trusty -> xyz
+			#14.04 trusty: (EOL: April 2019) lts: trusty -> xenial
 			unset warn_eol_distro
 			;;
 		quantal|raring|saucy)
