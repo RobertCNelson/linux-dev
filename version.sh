d11aed71a396ed62943be749986b9d81cb2e6360
Robert Nelson <robertcnelson@gmail.com>
2015-10-25 16:56:15 -0500

    kernel bump: v4.3-rc7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -15,13 +15,13 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.3
-KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=armv7-devel-r43
+KERNEL_TAG=${KERNEL_REL}-rc7
+BUILD=armv7-devel-r44
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="8a70dd2669200ce83255ed8c5ebef7e59f9e8707"
-KERNEL_SHA="03867292476e6fa7679395838d768dda0a0816c7"
+#prev_KERNEL_SHA="8a70dd2669200ce83255ed8c5ebef7e59f9e8707"
+#KERNEL_SHA="03867292476e6fa7679395838d768dda0a0816c7"
 
 #git branch
 #BRANCH="v4.3.x"
