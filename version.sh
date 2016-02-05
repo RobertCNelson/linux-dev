97e6f0cb83cd962d80c5f93f945e035a3ae11e58
Robert Nelson <robertcnelson@gmail.com>
2016-02-04 19:39:39 -0600

    tilcdc: pull in the 21 patches from ti-linux
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.5
 KERNEL_TAG=${KERNEL_REL}-rc2
-BUILD=${build_prefix}41
+BUILD=${build_prefix}42
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
