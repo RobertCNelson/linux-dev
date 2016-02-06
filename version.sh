7c98155045950084679518ea7f7761a44501d2e2
Robert Nelson <robertcnelson@gmail.com>
2016-02-05 20:01:19 -0600

    armv7-devel-r43: merge to: https://github.com/torvalds/linux/commit/dd6f86af394160f01f8b6c5349752b63b9e95fcb
    Compare: https://github.com/torvalds/linux/compare/b37a05c083c85c2657dca9bbe1f5d79dccf756d5...dd6f86af394160f01f8b6c5349752b63b9e95fcb
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,12 +25,12 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.5
 KERNEL_TAG=${KERNEL_REL}-rc2
-BUILD=${build_prefix}42
+BUILD=${build_prefix}43
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="36f90b0a2ddd60823fe193a85e60ff1906c2a9b3"
-KERNEL_SHA="b37a05c083c85c2657dca9bbe1f5d79dccf756d5"
+prev_KERNEL_SHA="b37a05c083c85c2657dca9bbe1f5d79dccf756d5"
+KERNEL_SHA="dd6f86af394160f01f8b6c5349752b63b9e95fcb"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
