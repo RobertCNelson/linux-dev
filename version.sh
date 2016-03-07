66cdcf3958b5b097ba38cb0f0b3136bc812f508f
Robert Nelson <robertcnelson@gmail.com>
2016-03-06 18:54:42 -0600

    kernel bump: v4.5-rc7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,13 +24,13 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.5
-KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=${build_prefix}54
+KERNEL_TAG=${KERNEL_REL}-rc7
+BUILD=${build_prefix}55
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="7d46af2084eabe73ce4dbf06072fded34b58b0c7"
-KERNEL_SHA="e3c2ef41f88e50c8557270868600d3132028af3b"
+#prev_KERNEL_SHA="7d46af2084eabe73ce4dbf06072fded34b58b0c7"
+#KERNEL_SHA="e3c2ef41f88e50c8557270868600d3132028af3b"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
