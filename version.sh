393dc3bc45fd3d73660129ddafee018ec250ead6
Robert Nelson <robertcnelson@gmail.com>
2016-03-14 19:45:34 -0500

    add: exynos
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.5
 KERNEL_TAG=${KERNEL_REL}
-BUILD=${build_prefix}57
+BUILD=${build_prefix}58
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
