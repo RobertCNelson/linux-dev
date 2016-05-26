2b44164544756a6ce5cc59a1cc7f50fa0db2a943
Robert Nelson <robertcnelson@gmail.com>
2016-05-26 11:50:53 -0500

    4.6-armv7-devel-r12: merge to: https://github.com/torvalds/linux/commit/2f7c3a18a2dc79ddf7b83ae199b099a675e1adb2
    Compare: https://github.com/torvalds/linux/compare/1d6da87a3241deb13d073c4125d19ed0e5a0c62c...2f7c3a18a2dc79ddf7b83ae199b099a675e1adb2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,12 +25,12 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.6
 KERNEL_TAG=${KERNEL_REL}
-BUILD=${build_prefix}11
+BUILD=${build_prefix}12
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="f6c658df63856db3bf8f467024b1dbee37b5399c"
-KERNEL_SHA="1d6da87a3241deb13d073c4125d19ed0e5a0c62c"
+prev_KERNEL_SHA="1d6da87a3241deb13d073c4125d19ed0e5a0c62c"
+KERNEL_SHA="2f7c3a18a2dc79ddf7b83ae199b099a675e1adb2"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
