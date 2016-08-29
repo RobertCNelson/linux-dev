fe7109f5e7f353c9cc0092df28ed01d09f9bac39
Robert Nelson <robertcnelson@gmail.com>
2016-08-29 09:12:26 -0500

    kernel bump: v4.8-rc4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,8 +27,8 @@ toolchain="gcc_linaro_gnueabihf_6"
 
 #Kernel/Build
 KERNEL_REL=4.8
-KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=${build_prefix}6
+KERNEL_TAG=${KERNEL_REL}-rc4
+BUILD=${build_prefix}7
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
