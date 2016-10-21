07776bca0586654eddad2bc0d8cbf140362def68
Robert Nelson <robertcnelson@gmail.com>
2016-10-21 14:53:52 -0500

    forward port: wl18xx regulator changes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}-rc1
-BUILD=${build_prefix}36
+BUILD=${build_prefix}37
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
