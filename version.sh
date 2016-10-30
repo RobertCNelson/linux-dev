ac91404fd70f42dfd17fa248c4b2d5418b256696
Robert Nelson <robertcnelson@gmail.com>
2016-10-29 22:41:58 -0500

    kernel bump: v4.9-rc3
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,13 +27,13 @@ toolchain="gcc_linaro_gnueabihf_6"
 
 #Kernel/Build
 KERNEL_REL=4.9
-KERNEL_TAG=${KERNEL_REL}-rc2
+KERNEL_TAG=${KERNEL_REL}-rc3
 BUILD=${build_prefix}39
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="07d9a380680d1c0eb51ef87ff2eab5c994949e69"
-KERNEL_SHA="ed99d3673e082b78b20208f346d2cd375483a779"
+#prev_KERNEL_SHA="07d9a380680d1c0eb51ef87ff2eab5c994949e69"
+#KERNEL_SHA="ed99d3673e082b78b20208f346d2cd375483a779"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
