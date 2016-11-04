7f2a2923ae509e2c9c64836b7b33e43f5aea29c0
Robert Nelson <robertcnelson@gmail.com>
2016-11-04 13:31:02 -0500

    drop: wlcore patch, doesnt fix it
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=${build_prefix}47
+BUILD=${build_prefix}48
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
