6fc0f3c7cb92ce944f65042bee3f8aa01bdf7638
Robert Nelson <robertcnelson@gmail.com>
2016-11-18 09:51:58 -0600

    4.9-rc5-armv7-devel-r61: merge to: https://github.com/torvalds/linux/commit/623898671c8eb05639e746e6d84cffa281616438
    Compare: https://github.com/torvalds/linux/compare/5d1904204c99596b50a700f092fe49d78edba400...623898671c8eb05639e746e6d84cffa281616438
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,12 +28,12 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=${build_prefix}60
+BUILD=${build_prefix}61
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="a25f0944ba9b1d8a6813fd6f1a86f1bd59ac25a6"
-KERNEL_SHA="5d1904204c99596b50a700f092fe49d78edba400"
+prev_KERNEL_SHA="5d1904204c99596b50a700f092fe49d78edba400"
+KERNEL_SHA="623898671c8eb05639e746e6d84cffa281616438"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
