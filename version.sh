485d85cc89febff7837ee95e4470fed44527b626
Robert Nelson <robertcnelson@gmail.com>
2016-11-23 13:09:44 -0600

    config: back to CONFIG_HZ_100
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=${build_prefix}62
+BUILD=${build_prefix}63
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
