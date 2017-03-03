bad1bb53902eda57239d6eb97f36d481b86ec7a6
Robert Nelson <robertcnelson@gmail.com>
2017-03-03 15:30:50 -0600

    4.10-armv7-devel-r9: merge to: https://github.com/torvalds/linux/commit/4e66c42c60fdf9be81837857454a41b39bf1b773
    Compare: https://github.com/torvalds/linux/compare/6053dc981449718d90a429933e99b441e1adaea6...4e66c42c60fdf9be81837857454a41b39bf1b773
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,12 +28,12 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.10
 KERNEL_TAG=${KERNEL_REL}
-BUILD=${build_prefix}8
+BUILD=${build_prefix}9
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="edccb59429657b09806146339e2b27594c1d1da0"
-KERNEL_SHA="6053dc981449718d90a429933e99b441e1adaea6"
+prev_KERNEL_SHA="6053dc981449718d90a429933e99b441e1adaea6"
+KERNEL_SHA="4e66c42c60fdf9be81837857454a41b39bf1b773"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
