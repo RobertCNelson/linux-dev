3d1f20689f8df575a02e9d5ea9ae04928044b023
Robert Nelson <robertcnelson@gmail.com>
2017-04-27 14:19:40 -0500

    am335x: build in video
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -448,9 +448,10 @@ beaglebone () {
 	fi
 
 	${git} "${DIR}/patches/beaglebone/dtbs/0001-sync-am335x-peripheral-pinmux.patch"
+	${git} "${DIR}/patches/beaglebone/dtbs/0002-dtc-fix-overlays.patch"
 
 	if [ "x${regenerate}" = "xenable" ] ; then
-		number=1
+		number=2
 		cleanup
 	fi
 
