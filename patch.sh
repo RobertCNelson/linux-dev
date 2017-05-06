363211cba22965e8ffd8ca15964b6bacf249152b
Robert Nelson <robertcnelson@gmail.com>
2017-05-05 21:52:34 -0500

    4.11-armv7-devel-r10: merge to: https://github.com/torvalds/linux/commit/3ef2bc099d1cce09e2844467e2ced98e1a44609d
    Compare: https://github.com/torvalds/linux/compare/2eecf3a49f1ff24c6116c954dd74e83f227fc716...3ef2bc099d1cce09e2844467e2ced98e1a44609d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -503,44 +503,6 @@ soc
 beaglebone
 dir 'build/gcc'
 
-sync_mainline_dtc () {
-	echo "dir: dtc"
-	#regenerate="enable"
-	if [ "x${regenerate}" = "xenable" ] ; then
-		cd ../
-		if [ -d ./dtc ] ; then
-			rm -rf ./dtc || true
-		fi
-
-		${git_bin} clone -b dtc-v1.4.4 https://github.com/RobertCNelson/dtc --depth=1
-
-		cd ./KERNEL/
-
-		sed -i -e 's:git commit:#git commit:g' ./scripts/dtc/update-dtc-source.sh
-		./scripts/dtc/update-dtc-source.sh
-		sed -i -e 's:#git commit:git commit:g' ./scripts/dtc/update-dtc-source.sh
-		git commit -a -m "scripts/dtc: Update to upstream version overlays" -s
-		git format-patch -1 -o ../patches/dtc/
-
-		rm -rf ../dtc/ || true
-
-		exit 2
-	else
-		#regenerate="enable"
-		if [ "x${regenerate}" = "xenable" ] ; then
-			start_cleanup
-		fi
-
-		${git} "${DIR}/patches/dtc/0001-scripts-dtc-Update-to-upstream-version-overlays.patch"
-
-		if [ "x${regenerate}" = "xenable" ] ; then
-			wdir="dtc"
-			number=1
-			cleanup
-		fi
-	fi
-}
-
 packaging () {
 	echo "dir: packaging"
 	#regenerate="enable"
@@ -554,6 +516,5 @@ packaging () {
 	fi
 }
 
-sync_mainline_dtc
 packaging
 echo "patch.sh ran successfully"
--- version.sh
+++ version.sh
@@ -28,12 +28,12 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.11
 KERNEL_TAG=${KERNEL_REL}
-BUILD=${build_prefix}9
+BUILD=${build_prefix}10
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="1a5fb64fee203f9f5a9274c67ddbb821a29f723f"
-KERNEL_SHA="2eecf3a49f1ff24c6116c954dd74e83f227fc716"
+prev_KERNEL_SHA="2eecf3a49f1ff24c6116c954dd74e83f227fc716"
+KERNEL_SHA="3ef2bc099d1cce09e2844467e2ced98e1a44609d"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
