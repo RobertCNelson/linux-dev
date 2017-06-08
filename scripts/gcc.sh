232fc1b8360e3b40870b7bb3b86606a9b8b8d63e
Robert Nelson <robertcnelson@gmail.com>
2017-06-08 10:16:12 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/gcc.sh
+++ scripts/gcc.sh
@@ -346,7 +346,7 @@ gcc_toolchain () {
 		#
 		#https://releases.linaro.org/components/toolchain/binaries/7.1-2017.05/aarch64-linux-gnu/gcc-linaro-7.1.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
 		#
-		site="https://snapshots.linaro.org"
+		#site="https://snapshots.linaro.org"
 
 		gcc_version="7.1"
 		gcc_minor=".1"
