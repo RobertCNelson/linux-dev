946cd05de1f7441303b8725a600302c275ac3a51
Robert Nelson <robertcnelson@gmail.com>
2017-06-19 10:32:03 -0500

    kernel bump: v4.12-rc6
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,8 +30,8 @@ toolchain="gcc_linaro_gnueabihf_7"
 
 #Kernel/Build
 KERNEL_REL=4.12
-KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=${build_prefix}29
+KERNEL_TAG=${KERNEL_REL}-rc6
+BUILD=${build_prefix}30
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
