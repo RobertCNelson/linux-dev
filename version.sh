beb794b264c6a042bbbd2f1d6b44cae25b4c3ec1
Robert Nelson <robertcnelson@gmail.com>
2017-07-30 15:41:40 -0500

    kernel bump: v4.13-rc3
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,8 +30,8 @@ toolchain="gcc_linaro_gnueabihf_7"
 
 #Kernel/Build
 KERNEL_REL=4.13
-KERNEL_TAG=${KERNEL_REL}-rc2
-BUILD=${build_prefix}19
+KERNEL_TAG=${KERNEL_REL}-rc3
+BUILD=${build_prefix}20
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
