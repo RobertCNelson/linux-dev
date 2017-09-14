ab7a2278d739908c9e671c89327de4d19c1812d9
Robert Nelson <robertcnelson@gmail.com>
2017-09-14 08:45:04 -0500

    4.13-armv7-devel-r20: merge to: https://github.com/torvalds/linux/commit/46c1e79fee417f151547aa46fae04ab06cb666f4
    Compare: https://github.com/torvalds/linux/compare/3882a734c19b3cd7feb9e30e1dbd8ae54ac0905a...46c1e79fee417f151547aa46fae04ab06cb666f4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -33,11 +33,11 @@ KERNEL_REL=4.13
 KERNEL_TAG=${KERNEL_REL}
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}19
+BUILD=${build_prefix}20
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="e7989f973ae1b90ec7c0b671c81f7f553affccbe"
-KERNEL_SHA="3882a734c19b3cd7feb9e30e1dbd8ae54ac0905a"
+prev_KERNEL_SHA="3882a734c19b3cd7feb9e30e1dbd8ae54ac0905a"
+KERNEL_SHA="46c1e79fee417f151547aa46fae04ab06cb666f4"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
