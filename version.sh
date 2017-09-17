d1bd7963ac40871c8015804ac479eb04846a3615
Robert Nelson <robertcnelson@gmail.com>
2017-09-16 21:52:05 -0500

    4.14-rc1-armv7-devel-r23: merge to: https://github.com/torvalds/linux/commit/
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -29,15 +29,15 @@ toolchain="gcc_linaro_gnueabihf_7"
 #toolchain="gcc_linaro_aarch64_gnu_7"
 
 #Kernel
-KERNEL_REL=4.13
-KERNEL_TAG=${KERNEL_REL}
+KERNEL_REL=4.14
+KERNEL_TAG=${KERNEL_REL}-rc1
 kernel_rt=".X-rtY"
 #Kernel Build
 BUILD=${build_prefix}23
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="711aab1dbb324d321e3d84368a435a78908c7bce"
-KERNEL_SHA="b38923a068c10fc36ca8f596d650d095ce390b85"
+#prev_KERNEL_SHA="711aab1dbb324d321e3d84368a435a78908c7bce"
+#KERNEL_SHA="b38923a068c10fc36ca8f596d650d095ce390b85"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
