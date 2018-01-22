d973fe237197d825d3e75af178c1814278d97dab
Robert Nelson <robertcnelson@gmail.com>
2018-01-21 20:53:08 -0600

    kernel bump: v4.15-rc9
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,7 +30,7 @@ toolchain="gcc_linaro_gnueabihf_7"
 
 #Kernel
 KERNEL_REL=4.15
-KERNEL_TAG=${KERNEL_REL}-rc8
+KERNEL_TAG=${KERNEL_REL}-rc9
 kernel_rt=".X-rtY"
 #Kernel Build
 BUILD=${build_prefix}22
