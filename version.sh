714c13286bdc1d68ed3e8fbc9258fb21958c556c
Robert Nelson <robertcnelson@gmail.com>
2018-01-29 13:45:24 -0600

    4.15-armv7-devel-r1: merge to: https://github.com/torvalds/linux/commit/47d5cc5be396eca67cc89572957ff16f10fd768e
    Compare: https://github.com/torvalds/linux/compare/c8f22b02a8bbc74525d17dd37d39bdf599e68a79...47d5cc5be396eca67cc89572957ff16f10fd768e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -33,11 +33,11 @@ KERNEL_REL=4.15
 KERNEL_TAG=${KERNEL_REL}
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}0
+BUILD=${build_prefix}1
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="d8a5b80568a9cb66810e75b182018e9edb68e8ff"
-KERNEL_SHA="c8f22b02a8bbc74525d17dd37d39bdf599e68a79"
+prev_KERNEL_SHA="c8f22b02a8bbc74525d17dd37d39bdf599e68a79"
+KERNEL_SHA="47d5cc5be396eca67cc89572957ff16f10fd768e"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
