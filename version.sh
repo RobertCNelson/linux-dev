0b55661ca2534fcb8ad6d50473a408a030badd41
Robert Nelson <robertcnelson@gmail.com>
2018-06-17 11:46:10 -0500

    kernel bump: v4.18-rc1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -29,15 +29,15 @@ toolchain="gcc_linaro_gnueabihf_7"
 #toolchain="gcc_linaro_aarch64_gnu_7"
 
 #Kernel
-KERNEL_REL=4.17
-KERNEL_TAG=${KERNEL_REL}
+KERNEL_REL=4.18
+KERNEL_TAG=${KERNEL_REL}-rc1
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}27
+BUILD=${build_prefix}28
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="4c5e8fc62d6a63065eeae80808c498d1dcfea4f4"
-KERNEL_SHA="35773c93817c5f2df264d013978e7551056a063a"
+#prev_KERNEL_SHA="4c5e8fc62d6a63065eeae80808c498d1dcfea4f4"
+#KERNEL_SHA="35773c93817c5f2df264d013978e7551056a063a"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
