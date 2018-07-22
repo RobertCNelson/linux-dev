fe53c5766ded16288ad36ecf2eac0a68382b4b4c
Robert Nelson <robertcnelson@gmail.com>
2018-07-22 17:30:25 -0500

    kernel bump: v4.18-rc6
    Signed-off-by: Your Name <you@example.com>

--- version.sh
+++ version.sh
@@ -30,7 +30,7 @@ toolchain="gcc_linaro_gnueabihf_7"
 
 #Kernel
 KERNEL_REL=4.18
-KERNEL_TAG=${KERNEL_REL}-rc5
+KERNEL_TAG=${KERNEL_REL}-rc6
 kernel_rt=".X-rtY"
 #Kernel Build
 BUILD=${build_prefix}31
