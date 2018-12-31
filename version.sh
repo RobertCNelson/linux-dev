d4ee21b8b722183acb83a8f8ca0ec90ea04f0a4d
Robert Nelson <robertcnelson@gmail.com>
2018-12-30 19:47:50 -0600

    4.20-armv7-devel-r0: merge to: https://github.com/torvalds/linux/commit/195303136f192d37b89e20a8d1d2670d0d825266
    Compare: https://github.com/torvalds/linux/compare/00c569b567c7f1f0da6162868fd02a9f29411805...195303136f192d37b89e20a8d1d2670d0d825266
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -38,8 +38,8 @@ kernel_rt=".X-rtY"
 BUILD=${build_prefix}0
 
 #v4.X-rcX + upto SHA
-prev_KERNEL_SHA="5694cecdb092656a822287a6691aa7ce668c8160"
-KERNEL_SHA="00c569b567c7f1f0da6162868fd02a9f29411805"
+prev_KERNEL_SHA="00c569b567c7f1f0da6162868fd02a9f29411805"
+KERNEL_SHA="195303136f192d37b89e20a8d1d2670d0d825266"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
