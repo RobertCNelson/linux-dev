f4c03d4e4b8da27d1e417ae123eacbec370d83c7
Robert Nelson <robertcnelson@gmail.com>
2019-01-02 08:59:51 -0600

    drop broken am335x-olimex-som.dtb
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -409,7 +409,6 @@ soc () {
 
 	dir 'soc/ti/blue'
 	dir 'soc/ti/abbbi'
-	dir 'soc/ti/am335x_olimex_som'
 	dir 'soc/ti/pocketbeagle'
 	dir 'soc/ti/beaglebone_capes'
 	dir 'soc/ti/uboot'
@@ -452,8 +451,6 @@ beaglebone () {
 #		device="am335x-boneblack-wireless-roboticscape.dtb" ; dtb_makefile_append
 
 		device="am335x-abbbi.dtb" ; dtb_makefile_append
-
-		device="am335x-olimex-som.dtb" ; dtb_makefile_append
 
 		device="am335x-boneblack-wl1835mod.dtb" ; dtb_makefile_append
 
