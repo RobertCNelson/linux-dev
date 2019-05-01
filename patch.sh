6bec1dcb63b1100a01910882ec8c24ec6f02761a
Robert Nelson <robertcnelson@gmail.com>
2019-05-01 15:47:41 -0500

    merge: soc/ti/abbbi to: https://github.com/beagleboard/BeagleBoard-DeviceTrees/tree/v5.1.x
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -320,6 +320,8 @@ beagleboard_dtbs () {
 
 		device="omap4-panda-es-b3.dtb" ; dtb_makefile_append_omap4
 
+		device="am335x-abbbi.dtb" ; dtb_makefile_append
+
 		${git_bin} add -f arch/arm/boot/dts/
 		${git_bin} add -f include/dt-bindings/
 		${git_bin} commit -a -m "Add BeagleBoard.org DTBS: $bbdtbs" -m "https://github.com/beagleboard/BeagleBoard-DeviceTrees/tree/${bbdtbs}" -s
@@ -447,7 +449,6 @@ soc () {
 	dir 'soc/imx/wandboard'
 	dir 'soc/imx/imx7'
 
-	dir 'soc/ti/abbbi'
 	dir 'soc/ti/pocketbeagle'
 	dir 'soc/ti/panda'
 	dir 'soc/ti/uboot'
@@ -459,8 +460,6 @@ beaglebone () {
 	echo "dir: beaglebone/generated"
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
-
-		device="am335x-abbbi.dtb" ; dtb_makefile_append
 
 		device="am335x-boneblack-uboot.dtb" ; dtb_makefile_append
 
