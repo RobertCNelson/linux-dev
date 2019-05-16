a8e4d86a451b29b3a28dd3791aa366769b1c637a
Robert Nelson <robertcnelson@gmail.com>
2019-05-16 13:58:29 -0500

    5.1-armv7-devel-r0: merge to: https://github.com/torvalds/linux/commit/a455eda33faafcaac1effb31d682765b14ef868c
    Compare: https://github.com/torvalds/linux/compare/35c99ffa20edd3c24be352d28a63cd3a23121282...a455eda33faafcaac1effb31d682765b14ef868c
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -38,8 +38,8 @@ kernel_rt=".X-rtY"
 BUILD=${build_prefix}0
 
 #v5.X-rcX + upto SHA
-prev_KERNEL_SHA="a13f0655503a4a89df67fdc7cac6a7810795d4b3"
-KERNEL_SHA="35c99ffa20edd3c24be352d28a63cd3a23121282"
+prev_KERNEL_SHA="35c99ffa20edd3c24be352d28a63cd3a23121282"
+KERNEL_SHA="a455eda33faafcaac1effb31d682765b14ef868c"
 
 #git branch
 #BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
