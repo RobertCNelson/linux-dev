b54070f707578ec786266e050aa7f346f23e8ba8
Robert Nelson <robertcnelson@gmail.com>
2020-02-20 16:44:53 -0600

    add config script for quick fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ tools/config-hot-fix.sh
@@ -0,0 +1,73 @@
+#!/bin/sh -e
+
+DIR=$PWD
+
+config_enable () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "xy" ] ; then
+		echo "Setting: ${config}=y"
+		./scripts/config --enable ${config}
+	fi
+}
+
+config_disable () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "xn" ] ; then
+		echo "Setting: ${config}=n"
+		./scripts/config --disable ${config}
+	fi
+}
+
+config_enable_special () {
+	test_module=$(cat .config | grep ${config} || true)
+	if [ "x${test_module}" = "x# ${config} is not set" ] ; then
+		echo "Setting: ${config}=y"
+		sed -i -e 's:# '$config' is not set:'$config'=y:g' .config
+	fi
+	if [ "x${test_module}" = "x${config}=m" ] ; then
+		echo "Setting: ${config}=y"
+		sed -i -e 's:'$config'=m:'$config'=y:g' .config
+	fi
+}
+
+config_module_special () {
+	test_module=$(cat .config | grep ${config} || true)
+	if [ "x${test_module}" = "x# ${config} is not set" ] ; then
+		echo "Setting: ${config}=m"
+		sed -i -e 's:# '$config' is not set:'$config'=m:g' .config
+	else
+		echo "$config=m" >> .config
+	fi
+}
+
+config_module () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "xm" ] ; then
+		echo "Setting: ${config}=m"
+		./scripts/config --module ${config}
+	fi
+}
+
+config_string () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "x${option}" ] ; then
+		echo "Setting: ${config}=\"${option}\""
+		./scripts/config --set-str ${config} "${option}"
+	fi
+}
+
+config_value () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "x${option}" ] ; then
+		echo "Setting: ${config}=${option}"
+		./scripts/config --set-val ${config} ${option}
+	fi
+}
+
+cd ${DIR}/KERNEL/
+
+#Nuke DSA SubSystem: 2020.02.20
+config="CONFIG_HAVE_NET_DSA" ; config_disable
+config="CONFIG_NET_DSA" ; config_disable
+
+cd ${DIR}/
